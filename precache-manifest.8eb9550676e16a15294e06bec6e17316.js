414a8cee514ce6482ed5818b3910479f0a58213e
pbolkas <p.bolkas@gmail.com>
2021-08-26 22:47:30 +0300

    Updates

---
+++ precache-manifest.8eb9550676e16a15294e06bec6e17316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041862fce8133c79a92b7b33ef654297",
+    "url": "/playlists_front_end/index.html"
+  },
+  {
+    "revision": "4f2af0019aa44610ebe5",
+    "url": "/playlists_front_end/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "5e7557fe0ee410203fed",
+    "url": "/playlists_front_end/static/js/2.3279cb54.chunk.js"
+  },
+  {
+    "revision": "4f2af0019aa44610ebe5",
+    "url": "/playlists_front_end/static/js/main.ced7e2d4.chunk.js"
+  },
+  {
+    "revision": "b1870882d3a823c38302",
+    "url": "/playlists_front_end/static/js/runtime~main.04c78143.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/playlists-app/precache-manifest.c859606c3571f3cfa716c4ce61d6c363.js"
+  "/playlists_front_end/precache-manifest.8eb9550676e16a15294e06bec6e17316.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playlists-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/playlists_front_end/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
